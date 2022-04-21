35691d382b00025bcd66f6df21d6c55f15aa6735
Janarbek <Janar24kalmyk@gmail.com>
2022-04-21 18:49:13 +0600

    add another commit

--- zadachki.js
+++ zadachki.js
@@ -78,36 +78,108 @@ function text(str){
         let arr=str.split("")
         console.log("===========================")
         console.log(arr);
-
         for (let i = 0; i < str.length; i++) {
-            for (let j = 0; j < 5; j++) {
-                if(arr[i]===a[j]){}
-                
+            if(arr[0]===a[i]){
+                console.log(arr[0]=b[i])
             }
-            
+            else if(arr[0]===b[i]){
+                console.log(arr[0]=c[i])
+            }
+            else if(arr[0]===c[i]){
+                console.log(arr[0]=d[i])
+            }
+            else if(arr[0]===d[i]){
+                console.log(arr[0]=e[i])
+            }
+            else if(arr[0]===e[i]){
+                console.log(arr[0]=a[i])
+            }
         }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        for (let j = 0; j < str.length; j++) {
+            if(arr[1]===a[j]){
+                console.log(arr[1]=b[j])
+            }
+            else if(arr[1]===b[j]){
+                console.log(arr[1]=c[j])
+            }
+            else if(arr[1]===c[j]){
+                console.log(arr[1]=d[j])
+            }
+            else if(arr[1]===d[j]){
+                console.log(arr[1]=e[j])
+            }
+            else if(arr[1]===e[j]){
+                console.log(arr[1]=a[j])
+            }
+        }
+        for (let q = 0; q < str.length; q++) {
+            if(arr[2]===a[q]){
+                console.log(arr[2]=b[q])
+            }
+            else if(arr[2]===b[q]){
+                console.log(arr[2]=c[q])
+            }
+            else if(arr[2]===c[q]){
+                console.log(arr[2]=d[q])
+            }
+            else if(arr[2]===d[q]){
+                console.log(arr[2]=e[q])
+            }
+            else if(arr[2]===e[q]){
+                console.log(arr[2]=a[q])
+            }
+        }
+        for (let l = 0; l < str.length; l++) {
+            if(arr[3]===a[l]){
+                console.log(arr[3]=b[l])
+            }
+            else if(arr[3]===b[l]){
+                console.log(arr[3]=c[l])
+            }
+            else if(arr[3]===c[l]){
+                console.log(arr[3]=d[l])
+            }
+            else if(arr[3]===d[l]){
+                console.log(arr[3]=e[l])
+            }
+            else if(arr[3]===e[l]){
+                console.log(arr[3]=a[l])
+            }
+        }
+        for (let p = 0; p < str.length; p++) {
+            if(arr[4]===a[p]){
+                console.log(arr[4]=b[p])
+            }
+            else if(arr[4]===b[p]){
+                console.log(arr[4]=c[p])
+            }
+            else if(arr[4]===c[p]){
+                console.log(arr[4]=d[p])
+            }
+            else if(arr[4]===d[p]){
+                console.log(arr[4]=e[p])
+            }
+            else if(arr[4]===e[p]){
+                console.log(arr[4]=a[p])
+            }
+        }
+        for (let k = 0; k < str.length; k++) {
+            if(arr[5]===a[k]){
+                console.log(arr[5]=b[k])
+            }
+            else if(arr[5]===b[k]){
+                console.log(arr[5]=c[k])
+            }
+            else if(arr[5]===c[k]){
+                console.log(arr[5]=d[k])
+            }
+            else if(arr[5]===d[k]){
+                console.log(arr[5]=e[k])
+            }
+            else if(arr[5]===e[k]){
+                console.log(arr[5]=a[k])
+            }
+        }
     }
 
     array1=["a","b","c","d","e"]
